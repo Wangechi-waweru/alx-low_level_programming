5d3c9ef075137e30c40fdab2b92cdd31efa0b00b
Wangechi-waweru <wawerulynn@gmail.com>
2022-04-09 07:25:25 -0700

    Task 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-/**
-  *main -Beggining of function
-  *
-  *Return: Function ends
-  */
-int main(void)
-{
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof (d));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof (a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof (b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); 
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(f)); 
-	return (0);
+
+
+/**
+
+  * main - Function begins
+
+  *
+
+  * Return: Function ends
+
+  */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
