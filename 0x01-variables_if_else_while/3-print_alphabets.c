4952285ab5f05a4f0bb5c88f2e9bbb2a2567b418
Wangechi-waweru <wawerulynn@gmail.com>
2022-04-10 15:15:26 -0700

    Task 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
-  * main -entry point
+  * main - Entry point
   *
   * Return - always 0
   */
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return - always 0
+  */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10; digit++)
+		printf("%digit", digit);
+		printf("\n");
+
+	return (0);
+}
