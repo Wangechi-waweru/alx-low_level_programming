5be908e9ccf949e7ab7a79e4b6f349da3068172a
Wangechi-waweru <wawerulynn@gmail.com>
2022-04-11 06:46:14 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main : Entry point
- *
- * Return: always 0
+/**
+ * main -assigns a random number
+ * it executes and prtints
+ * Return: Always 0
 */
 
 int main(void)
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 48 ; i <= 57; i++);
+	for (i = 0 ; i <= 9; i++);
 
 	putchar (i);
+
 	putchar ('\n');
 
 	return (0);
