e7d9ec566f75caa7a7f282ea57d8aa01bfd966b7
Wangechi-waweru <wawerulynn@gmail.com>
2022-04-24 05:02:10 -0700

    Task 3: function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,14 @@ i  * @s1: first string to compare
   */
 int _strcmp(char *s1, char *s2)
 {
-	int j = 0, k = 0;
-
-	while (k == 0)
+	while (*s1 == *s2)
 	{
-		if ((*(s1 + 1) == '\0') && (*(s2 + j) == '\0'))
-			break;
-		k = *(s1 + 1) - *(s2 + j);
-		j++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	return (k);
+	return (*s1 - *s2);
 }
