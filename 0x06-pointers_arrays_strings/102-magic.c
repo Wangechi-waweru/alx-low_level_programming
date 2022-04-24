3c7a6f76dd0a73b712d3df45643df868547339e5
Wangechi-waweru <wawerulynn@gmail.com>
2022-04-24 05:38:23 -0700

    Task 10: Function that prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
-  * main: returns void
-  * @n,a and p are all integers
-  * Return: 0 if correct
-  */
 
 int main(void)
 {
@@ -11,9 +6,9 @@ int main(void)
 	int a[5];
 	int *p;
 
-	 a[2] = 1024;
-	 p = &n;
-	 /*
+	a[2] = 1024;
+	p = &n;
+	/**
 	  * write your line of code here...
 	  * Remember:
 	  * - you are not allowed to use a
@@ -21,8 +16,8 @@ int main(void)
 	  * - only one statement
 	  * - you are not allowed to code anything else than this line of code
 	  */
-	 *(p + 5) = 98;
-	 /* ...so that this prints 98\n */
-	 printf("a[2] = %d\n", a[2]);
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
