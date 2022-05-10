e4e6cb18c59df9e7d1fda4089b32c08ef7f5fdb4
Wangechi-waweru <wawerulynn@gmail.com>
2022-05-10 13:48:38 -0700

    Task 5: checks if a string contains a non-digit char, returns the length of a string, handles errors for main and multiplies two positive numbers.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -9,6 +12,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
-int is_digit(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
