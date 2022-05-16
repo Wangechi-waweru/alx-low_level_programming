248af9e714de5cc4678048bf55abdb257cffbfbe
Wangechi-waweru <wawerulynn@gmail.com>
2022-05-16 14:39:56 -0700

    Program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,38 +9,19 @@
   */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	register int a, b;
+	int (*fptr)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
+	fptr = get_op_func(argv[2]);
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	if (!fptr)
+		printf("Error\n"), exit(99);
 
-	func = get_op_func(argv[2]);
-
-	if (!func)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", fptr(a, b));
 
 	return (0);
 }
