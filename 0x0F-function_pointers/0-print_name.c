af1dcc5037387485dffee2cac24dbd3f03b26007
Wangechi-waweru <wawerulynn@gmail.com>
2022-05-16 13:21:54 -0700

    Task 0: prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+  * print_name - prints a name
+  * @name: name to print
+  * @f: pointer to the printing function
+  *
+  * Return: Nothing
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
