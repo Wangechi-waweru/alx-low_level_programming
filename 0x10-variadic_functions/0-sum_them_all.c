4d22ea346d6afe08221099f83a7e26e897fea57f
Wangechi-waweru <wawerulynn@gmail.com>
2022-05-17 04:55:12 -0700

    Task 0: Task that returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "variadic_fuctions.h"
+#include "variadic_functions.h"
 
 /**
   * sum_them_all - calculates the sum of all its parameters
@@ -8,7 +8,7 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	register unsigned int i;
+	unsigned int i;
 	int sum = 0;
 	va_list arguments;
 
