e4ddd7f955789c4faa7e1e4e1569b743acf9578e
Wangechi-waweru <wawerulynn@gmail.com>
2022-06-16 04:46:09 -0700

    Updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
+char *_initialize_array(char *ar, int lar)
 
 #endif
