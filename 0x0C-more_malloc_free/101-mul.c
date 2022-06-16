b8d1b40fd83ff1e7159847b9e6c42e3303f48f80
Wangechi-waweru <wawerulynn@gmail.com>
2022-06-16 04:32:16 -0700

    Last commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * is_digit - checks if a string contains a non-digit char
-  * @s: string to be evaluated
-  *
-  * Return: 0 if a non-digit is found and 1 otherwise
-  */
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
 int is_digit(char *s)
 {
 	int i = 0;
@@ -22,16 +22,16 @@ int is_digit(char *s)
 }
 
 /**
-  * _strlen - returns the length of a string
-  * @s: string to evaluate
-  *
-  * Return: the length of the string
-  */
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] = '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -39,8 +39,8 @@ int _strlen(char *s)
 }
 
 /**
-  * errors - handles errors for main
-  */
+ * errors - handles errors for main
+ */
 void errors(void)
 {
 	printf("Error\n");
@@ -48,12 +48,12 @@ void errors(void)
 }
 
 /**
-  * main - multiplies two positive numbers
-  * @argc: number of arguments
-  * @argv: array of arguments
-  *
-  * Return: always 0
-  */
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
@@ -97,4 +97,3 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
-
